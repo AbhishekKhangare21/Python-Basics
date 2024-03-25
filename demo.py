b2de8c6269bec778288aa06cf09ee4e2fdfe12d5
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-25 16:07:29 +0530

    Tool tip calculator

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print("One day I will become the number 1 AI engineer in the world");
+print('Welcome to the tip calculator!')
+bill = float(input("What as the total bill? $"))
+tip = input("How much tip would you like to give? 10, 12, or 15? ")
+people = int(input("How many people to split the bill?"))
+bill_with_tip = bill * (1 + tip / 100)
+
+
+print(bill_with_tip);
