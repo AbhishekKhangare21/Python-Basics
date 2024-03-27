2bfaa7ff107d38a73644d180bf3f5f9acca9defa
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-27 07:25:03 +0530

    16. Day 1 Project: Band Name Generator

---
+++ Day 1/16.py
@@ -0,0 +1,9 @@
+print("Welcome to band name generator.")
+
+city = input("Which city did you grow up in?")
+print(city)
+
+pet = input("What is the name of a pet?")
+
+print("Your band name could be " + city + " " + pet)
+
