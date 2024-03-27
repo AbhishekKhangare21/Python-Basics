67a0f648f2e1b36acf878817a9bb8a6a51d3a502
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-27 06:35:18 +0530

    Delete tip calculator script
    The `demo.py` script has been deleted. It was a simple tip calculator that took in the total bill amount, tip percentage, and number of people to split the bill, then calculated and printed the bill with tip included.

--- demo.py
+++
@@ -1,8 +0,0 @@
-print('Welcome to the tip calculator!')
-bill = float(input("What as the total bill? $"))
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
-people = int(input("How many people to split the bill?"))
-bill_with_tip = bill * (1 + tip / 100)
-
-
-print(bill_with_tip);
