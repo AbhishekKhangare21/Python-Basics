553c4db20bbb3fee6c0658d90db8b5487aee5d04
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-28 09:13:23 +0530

    33. BMI 2.0

---
+++ Day 3/33.py
@@ -0,0 +1,8 @@
+height = float(input())
+weight = int(input())
+
+bmi = weight / (height * height)
+if bmi < 18.5:
+    print("Your BMI is {bmi}, you are underwieght.")
+elif bmi < 25:
+    print("Your BMI is {bmi}, you have a normal weight.")
