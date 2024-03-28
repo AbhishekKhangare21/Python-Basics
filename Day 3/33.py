a1446a0b6ddc713ef5924dfeca1471efeb8c6d46
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-28 09:15:53 +0530

    33. BMI 2.0

--- Day 3/33.py
+++ Day 3/33.py
@@ -6,3 +6,7 @@ if bmi < 18.5:
     print("Your BMI is {bmi}, you are underwieght.")
 elif bmi < 25:
     print("Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 35:
+    print ("Your BMI is {bmi}, you are obese.")
+else:
+    print("Your BMI is {bmi}, you are clinically obese.")
