5ab467159cbf4bea325d9dda059fc41521696a64
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-29 08:31:10 +0530

    46. IndexErros and Working with Nested Lists

---
+++ Day 4/46.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Banana", "Orange", "Grape", "Pineapple", "Strawberry", "Mango"]
+
+vegetables = ["Carrot", "Broccoli", "Spinach", "Tomato", "Potato"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
